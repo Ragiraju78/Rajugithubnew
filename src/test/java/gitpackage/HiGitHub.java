452759e11bde37114248b7ae99a2c36e64c2f4de
Raju <ragiraju78@gmail.com>
2020-08-06 13:23:39 +0200

    my fifth commit

--- src/test/java/gitpackage/HiGitHub.java
+++ src/test/java/gitpackage/HiGitHub.java
@@ -4,5 +4,6 @@ public class HiGitHub {
     public static void main(String[] args) {
         System.out.println("Hi GitHub This is Raju");
         System.out.println("Welcome to CI");
+        System.out.println("Check CI");
     }
 }
