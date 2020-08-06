ddb46053b2fc8c4b25c5249af9f1ffb9bbd580f8
Raju <ragiraju78@gmail.com>
2020-08-06 12:33:07 +0200

    my third commit

--- src/test/java/gitpackage/HiGitHub.java
+++ src/test/java/gitpackage/HiGitHub.java
@@ -3,5 +3,6 @@ package gitpackage;
 public class HiGitHub {
     public static void main(String[] args) {
         System.out.println("Hi GitHub This is Raju");
+        System.out.println("Welcome to CI");
     }
 }
